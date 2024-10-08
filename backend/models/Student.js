6247ffea791de5d2c80c30002638ad29f642fa54
theamantaneja <theamantaneja@gmail.com>
2024-10-08 12:58:27 +0530

    model updates

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
+  name: { type: String, required: false },
+  age: { type: Number, required: false },
   class: { type: String, required: true },
   stream: { type: String, required: true, default: "general"},
-  school: { type: String, required: true },
+  school: { type: String, required: false },
   board: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  mobile: { type: String, required: true },
+  email: { type: String, required: false, unique: true },
+  mobile: { type: String, required: false },
   // Stream field
 }, {
   timestamps: true
